64b2755c4beaa714e354ec8bd6cda68e29f245ee
Matheus Lima <contato@matheuslima.com.br>
2019-07-21 23:28:00 -0400

    Added file upload

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const router = new Router();
+const upload = multer(multerConfig);
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hello world from MeetApp.' });
@@ -21,5 +25,7 @@ router.post('/sessions', SessionController.store);
 router.use(authMiddleware);
 
 router.put('/users', UserController.update);
+
+router.post('/files', upload.single('file'), FileController.store);
 
 export default router;
