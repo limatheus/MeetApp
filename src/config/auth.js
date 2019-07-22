e745800b52d92f6e49275fd0f7b96c7b15fa69aa
Matheus Lima <contato@matheuslima.com.br>
2019-07-21 22:37:11 -0400

    Added auth and user update

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'aa6de828b3d53ce7e6390769498e3061',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const router = new Router();
 
@@ -9,5 +12,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/users', UserController.store);
+
+router.post('/sessions', SessionController.store);
+
+/**
+ * Routes bellow need authorization
+ */
+router.use(authMiddleware);
+
+router.put('/users', UserController.update);
 
 export default router;
