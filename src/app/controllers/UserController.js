ea316d4bae2259194d49c5021dbb74380b36f24e
Matheus Lima <contato@matheuslima.com.br>
2019-07-21 22:16:34 -0400

    Added database and user entity

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,36 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'The validation fails.' });
+    }
+
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      return res.status(400).json({ error: 'User already exist.' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Hello world from MeetApp' });
+  res.json({ message: 'Hello world from MeetApp.' });
 });
+
+router.post('/users', UserController.store);
 
 export default router;
